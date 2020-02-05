2649b33f65d5597ea49bb04d4a6a0b7259a19ecc
mahmoud <mahmoud32salamn>
2020-02-05 15:16:46 +0200

    add signIn and SignUp components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import SignUpFormComponent from './components/SignUpComponent';
+import SignInFormComponent from './components/SginInComponent'
+
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+              <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div className="FormTitle">
+              <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpFormComponent}>
+            </Route>
+            <Route path="/sign-in" component={SignInFormComponent}>
+            </Route>
+          </div>
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
