4da93a56b4ccf7be7515afded0e2daeb24cd4402
mahmoud 32salamn <mahmoud32salamn@gmail.com>
2020-08-16 07:39:11 +0200

    make DB live

--- configerations/dbConfig.js
+++ configerations/dbConfig.js
@@ -1,25 +1,43 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.Promise = global.Promise;
 
 var uri = process.env.localUri;
 
 if (process.env.NODE_ENV === "production") {
+  //   uri = encodeURI(process.env.ATLAS_URI);
 
-    uri = encodeURI(process.env.ATLAS_URI);
+  mongoose
+    .connect(encodeURI(process.env.ATLAS_URI), {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+} else {
+  // console.log('uri  :' + process.env.localUri);
+  // mongoose.connect(uri, {
+  //     useUnifiedTopology: true,
+  //     useNewUrlParser: true,
+  //     useCreateIndex: true
+  // }).catch(e => {
+  //     console.log(e);
+  // });
 
+  mongoose
+    .connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
-// console.log('uri  :' + process.env.localUri);
-mongoose.connect(uri, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-}).catch(e => {
-    console.log(e);
-});
+
 var db = mongoose.connection;
 
 module.exports = db;
-
-
